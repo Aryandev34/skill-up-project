3c929bd9f860f4e5306759e1a45dd045baad30dd
Aryan <aryantanwar0821@gmail.com>
2026-09-09 14:01:24 +0530

    Configure WhiteNoise for Vercel static files

--- skillup/settings.py
+++ skillup/settings.py
@@ -49,6 +49,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this line
+    'django.contrib.sessions.middleware.SessionMiddleware',
 ]
 
 ROOT_URLCONF = 'skillup.urls'
@@ -118,7 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_URL = 'quiz:login'
 LOGIN_REDIRECT_URL = 'quiz:dashboard'
 LOGOUT_REDIRECT_URL = 'quiz:landing'
